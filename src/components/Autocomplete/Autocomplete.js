9de0cd0e159911272263ec47949084050ae4c619
Kamil Szerląg <kamilszerlag@gmail.com>
2021-02-24 19:58:34 +0100

    add Autocomplete with SearchBox, add Home page

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -21,7 +21,7 @@ const Autocomplete = () => {
 	};
 
 	const apiUrl = `https://api.unsplash.com/search/photos?per_page=30&query=${inputValue}`;
-	const _apiKey = 'IiHTjYC5n1BhVTDfhpUAo-m5H1qPHy4CXT-WfrMDO4A';
+	const _apiKey = 'xxx';
 
 	useEffect(() => {
 		fetch(apiUrl, {
