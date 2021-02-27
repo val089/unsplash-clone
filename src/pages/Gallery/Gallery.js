1c92c345d4773ba436164add507a712b4492bf87
Kamil Szerląg <kamilszerlag@gmail.com>
2021-02-27 12:39:48 +0100

    refactor Autocomplete

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './Gallery.scss';
+import { Link } from 'react-router-dom';
 
 const Gallery = ({ searchValue }) => {
-	const [photos, setPhotos] = useState();
+	const [photos, setPhotos] = useState([]);
 
 	const apiUrl = `https://api.unsplash.com/search/photos?per_page=30&query=${searchValue}&orientation=squarish`;
 	const _apiKey = 'IiHTjYC5n1BhVTDfhpUAo-m5H1qPHy4CXT-WfrMDO4A';
@@ -22,16 +23,20 @@ const Gallery = ({ searchValue }) => {
 			});
 	}, [apiUrl]);
 
-	return (
-		<div className="gallery">
-			{photos &&
-				photos.map((item) => (
-					<div key={item.id} className="photo">
-						<img src={item.urls.regular} alt="me" />
-					</div>
-				))}
-		</div>
-	);
+	if (searchValue) {
+		return (
+			<div className="gallery">
+				{photos &&
+					photos.map((item) => (
+						<div key={item.id} className="photo">
+							<img src={item.urls.regular} alt="me" />
+						</div>
+					))}
+			</div>
+		);
+	} else {
+		return <Link to="/">Back</Link>;
+	}
 };
 
 export default Gallery;
